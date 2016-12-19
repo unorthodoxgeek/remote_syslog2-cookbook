25333ca7e64a37efa6e1bde48efaf7a0e9cd14f5
Tom Caspy <tcaspy@gmail.com>
2016-12-19 15:36:22 +0200

    allowing multiple remote_syslog instances

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -3,13 +3,17 @@
 # https://tickets.opscode.com/browse/CHEF-3953
 # Also, convert port to integer since remote_syslog2 does not accept quoted port
 
-config_options = JSON.parse(node['remote_syslog2']['config'].to_json)
-config_options['destination']['port'] = Integer(config_options['destination']['port'])
-config_options = config_options.to_yaml
+config_options = JSON.parse(node['remote_syslog2']['config'].to_json).to_a
 
-file node['remote_syslog2']['config_file'] do
-  content config_options
+config_options.each_with_index do |config, index|
+  config['destination']['port'] = config['destination']['port'].to_i
+  yaml_config = config.to_yaml
+  suffix = index > 0 ? "_#{index}" : ""
 
-  mode '0644'
-  notifies :restart, 'service[remote_syslog2]', :delayed
+  file "#{node['remote_syslog2']['config_file']}#{suffix}" do
+    content yaml_config
+
+    mode '0644'
+    notifies :restart, "service[remote_syslog2#{suffix}]", :delayed
+  end
 end
--- recipes/service.rb
+++ recipes/service.rb
@@ -1,12 +1,17 @@
-template '/etc/init.d/remote_syslog2' do
-  source 'remote_syslog2.erb'
-  mode '0755'
-  notifies :restart, 'service[remote_syslog2]', :delayed
-end
+node['remote_syslog2']['config'].to_a.each_with_index do |index, conf|
+  suffix = index > 0 ? "_#{index}" : ""
 
-service 'remote_syslog2' do
-  supports restart: true, status: true
-  action [:start, :enable]
+  template "/etc/init.d/remote_syslog2#{suffix}" do
+    source 'remote_syslog2.erb'
+    mode '0755'
+    variables({suffix: suffix})
+    notifies :restart, "service[remote_syslog2#{suffix}]", :delayed
+  end
+
+  service "remote_syslog2#{suffix}" do
+    supports restart: true, status: true
+    action [:start, :enable]
+  end
 end
 
 
